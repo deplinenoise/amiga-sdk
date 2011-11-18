cc6c01ce793932352d5b2a3ac8834844a9e69ccd
Andreas Fredriksson <dep@defmacro.se>
2011-11-18 10:21:54 +0100

    Use Kalms's hacked cia_protos that actually compiles.

--- sdkinclude/inline/cia_protos.h
+++ sdkinclude/inline/cia_protos.h
@@ -1,3 +1,15 @@
-struct Interrupt * __AddICRVector(__reg("a6") struct Library *resource,__reg("d0") long iCRBit,__reg("a1") struct Interrupt *interrupt ,__reg("a6") void *)="\tjsr\t-6(a6)";
-#define AddICRVector(x1,x2,x3) __AddICRVector((x1),(x2),(x3),CardResource)
-No base defined!
+// homemade by Kalms
+
+#ifndef _VBCCINLINE_EXEC_H
+#define _VBCCINLINE_CIA_H
+
+#ifndef EXEC_TYPES_H
+#include <exec/types.h>
+#endif
+
+struct Interrupt *AddICRVector(__reg("a6") struct Library *resource, __reg("d0") long iCRBit, __reg("a1") struct Interrupt *interrupt) = "\tjsr\t-6(a6)";
+void RemICRVector(__reg("a6") struct Library *resource, __reg("d0") long iCRBit, __reg("a1") struct Interrupt *interrupt) = "\tjsr\t-12(a6)";
+WORD AbleICR(__reg("a6") struct Library *resource, __reg("d0") long mask ) = "\tjsr\t-18(a6)";
+WORD SetICR(__reg("a6") struct Library *resource, __reg("d0") long mask ) = "\tjsr\t-24(a6)"; 
+
+#endif /*  _VBCCINLINE_CIA_H  */
